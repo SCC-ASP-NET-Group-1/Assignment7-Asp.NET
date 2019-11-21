854a88c55be77dbd95f8e1a9718289e7bfe8ce09
Hunter <hl573929@southeast.edu>
2019-11-21 13:11:59 -0600

    added listings controller and made the pages for listings

---
+++ Assignment7/Assignment7/Controllers/ListingsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Assignment7.Data;
+using Assignment7.Models;
+
+namespace Assignment7.Controllers
+{
+    public class ListingsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ListingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Listings
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Listings.ToListAsync());
+        }
+
+        // GET: Listings/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listings
+                .SingleOrDefaultAsync(m => m.ListingID == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            return View(listing);
+        }
+
+        // GET: Listings/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Listings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ListingID,Name,Description,Price,Username,ZipCode")] Listing listing)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(listing);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(listing);
+        }
+
+        // GET: Listings/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listings.SingleOrDefaultAsync(m => m.ListingID == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+            return View(listing);
+        }
+
+        // POST: Listings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ListingID,Name,Description,Price,Username,ZipCode")] Listing listing)
+        {
+            if (id != listing.ListingID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(listing);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ListingExists(listing.ListingID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(listing);
+        }
+
+        // GET: Listings/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var listing = await _context.Listings
+                .SingleOrDefaultAsync(m => m.ListingID == id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            return View(listing);
+        }
+
+        // POST: Listings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var listing = await _context.Listings.SingleOrDefaultAsync(m => m.ListingID == id);
+            _context.Listings.Remove(listing);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ListingExists(int id)
+        {
+            return _context.Listings.Any(e => e.ListingID == id);
+        }
+    }
+}
